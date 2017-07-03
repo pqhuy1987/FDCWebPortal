61617704ae5d763c4b3eeecb8698425b0cfc0c25
pqhuy1987 <pqhuy1987@gmail.com>
2017-07-03 18:16:11 +0700

    Update cho Linux Server

--- admin/menu.php
+++ admin/menu.php
@@ -1,5 +1,5 @@
-<a href="./">TRANG CHỦ</a>
+<a href="./">LỊCH CÔNG TY</a>
 <a href="./listTheLoai.php">QUẢN LÝ THỂ LOẠI</a>
 <a href="./listLoaiTin.php">QUẢN LÝ LOẠI TIN</a>
 <a href="./listTin.php">QUẢN LÝ TIN TỨC</a>
-<a href="./listQuangCao.php">QUẢN LÝ FILE VÀ TÀI LIỆU</a>
+<a href="./listquangcao.php">QUẢN LÝ FILE VÀ TÀI LIỆU</a>
