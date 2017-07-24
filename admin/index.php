40192133539beb702849b248122e1d05898ddd6b
pqhuy1987 <pqhuy1987@gmail.com>
2017-07-24 16:14:25 +0700

    Xóa thêm lịch

--- admin/index.php
+++ admin/index.php
@@ -64,56 +64,10 @@ $(document).ready(function() {
         <td width="80"><a href="themTin.php">Thêm</a></td>
       </tr>
 
-			<?php
-            $sotin1trang = 50;
-        
-            if (isset($_GET["trang"]))
-            {
-                $trang = $_GET["trang"];
-                settype($trang, "int");
-            }else {
-                $trang = 1;
-            }
-        
-            $from = ($trang - 1)*$sotin1trang;
-    
-            $ChonTin_Theo_PhanTrang = ChonTin_Theo_PhanTrang($connect, $from, $sotin1trang);
-            
-            while ($row_ChonTin_Theo_PhanTrang = mysqli_fetch_array($ChonTin_Theo_PhanTrang))
-            {
-            ?>
-          <tr>
-            <td>idLich::<?php echo $row_ChonTin_Theo_PhanTrang["idTin"]?></td>
-            <td><a href="suaTin.php?idTin=<?php echo $row_ChonTin_Theo_PhanTrang["idTin"]?>"><?php echo $row_ChonTin_Theo_PhanTrang["TieuDe"]?></a><br />
-              <?php echo $row_ChonTin_Theo_PhanTrang["TomTat"]?></td>
-            <td>&nbsp;</td>
-            <td><a href="suaTin.php?idTin=<?php echo $row_ChonTin_Theo_PhanTrang["idTin"]?>">Sửa</a> - <a onclick="return confirm('Bạn có chắc muốn xóa?')" href="xoaTin.php?idTin=<?php echo $row_ChonTin_Theo_PhanTrang["idTin"]?>">Xóa</a></td>
-          </tr>
-          <?php 
-            }
-          ?>
+
         </table></td>
   </tr>
     </table>
-	<style>
-        #phantrang {text-align: center; margin:15px auto; overflow:auto}
-        #phantrang a {background-color: #000; color:#ff0; padding:5px; margin-right: 3px;}
-        #phantrang a:hover{background-color:#09f}
-    </style>
-    <div id="phantrang">
-		<?php 
-            $Tongsotin = ChonTin_TatCa($connect);
-            $row_Tongsotin = mysqli_num_rows($Tongsotin);
-            $tongsotrang =  ceil($row_Tongsotin/$sotin1trang);
-            for ($i = 1; $i <=$tongsotrang; $i++)
-			{
-        ?>
-        
-        	<a <?php if($i==$trang) echo "style='background-color:red' "; ?> href="listTin.php?p=&trang=<?php echo $i?>"><?php echo $i ?></a>
-        
-        <?php 
-            }
-        ?>
-    </div>
+
 </body>
 </html>
