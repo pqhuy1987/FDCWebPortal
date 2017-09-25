03a1102e8eabde11b7b42a7f0b76e315eb2b12a9
pqhuy1987 <pqhuy1987@gmail.com>
2017-09-25 15:05:43 +0700

    Sửa phần sửa câu hỏi.

--- fdc_test/admin/settings.php
+++ fdc_test/admin/settings.php
@@ -14,18 +14,11 @@ $eid=$_GET['eid'];
 
 if($eid!="")
 {
-	$edit_res 		= 	mysqli_query($connect_2,"SELECT * FROM settings where id='$eid'");
+	$edit_res 		= 	mysqli_query($connect_2,"SELECT * FROM quiz where id='$eid'");
 	$row			=	mysqli_fetch_assoc($edit_res);
 	$exam_name		=	trim($row['exam_name']);
-	$opt1			=	trim($row['opt1']);
-	
-	$opt2		=	trim($row['opt2']);
-	$opt3		=	trim($row['opt3']);
-	$opt4		=	trim($row['opt4']);
-	$answer		=	trim($row['answer']);
-	$catidd		=	$row['catid'];
-	$dokho		=	$row['dokho'];
-	$id_sub		=	$row['id_sub'];
+	$pagenum		=	trim($row['pagenum']);
+	$examtime		=	trim($row['examtime']);
 	
 	$edit_cat_name = mysqli_query($connect_2,"SELECT * FROM category where id='$catidd'");
 	$cat_row=mysqli_fetch_assoc($edit_cat_name);
